d0fb563aec0616fce65e13200d911065e7b38721
PatrickMamsery <pkrobert1612@gmail.com>
2023-09-15 07:18:55 +0300

    wip: minor changes to customer list rendering from API data

--- controlers/customer-list.js
+++ controlers/customer-list.js
@@ -5,15 +5,15 @@ function populateTable(data) {
     data.forEach(customer => {
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${customer.customer_id}</td>
-            <td>${customer.firstName} ${customer.LastName}</td>
+            <td>${customer.id}</td>
+            <td>${customer.firstName} ${customer.lastName}</td>
             <td>${customer.userName}</td>
             <td>${customer.email}</td>
             <td>${customer.phone}</td>
             <td>${customer.address}</td>
             <td>${customer.date_of_birth}</td>
             <td>${customer.gender}</td>
-            <td>${customer.national_id}</td>
+            <td>${customer.nidaId}</td>
         `;
         tableBody.appendChild(row);
     });
